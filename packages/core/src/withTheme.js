a6417fc8c8019dd36c3c8a70789c2c9a58abc1a2
Shengdong Ye <38345792@qq.com>
2020-04-20 13:48:10 +0800

    CMUFC-1822[1] Enable validate form programmatically for MuiForm (#3)
    Co-authored-by: Allen Ye <allen.ye@thomsonreuters.com>

--- packages/core/src/withTheme.js
+++ packages/core/src/withTheme.js
@@ -1,11 +1,13 @@
-import React, { forwardRef } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Form from "./";
 
 function withTheme(themeProps) {
-  return forwardRef(({ fields, widgets, ...directProps }, ref) => {
+  return forwardRef(({ fields, widgets, formRef, ...directProps }, ref) => {
     fields = { ...themeProps.fields, ...fields };
     widgets = { ...themeProps.widgets, ...widgets };
+
+    const currentRef = formRef ? formRef : ref;
 
     return (
       <Form
@@ -13,7 +15,7 @@ function withTheme(themeProps) {
         {...directProps}
         fields={fields}
         widgets={widgets}
-        ref={ref}
+        ref={currentRef}
       />
     );
   });
@@ -22,6 +24,11 @@ function withTheme(themeProps) {
 withTheme.propTypes = {
   widgets: PropTypes.object,
   fields: PropTypes.object,
+  formRef: PropTypes.object,
+};
+
+withTheme.defaultProps = {
+  formRef: null,
 };
 
 export default withTheme;
